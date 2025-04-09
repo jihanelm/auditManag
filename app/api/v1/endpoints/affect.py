874e112bddcfb0a57db424eff18ae2cc45a1dcff
jihanelm <jihane.elmghaoui@gmail.com>
2025-04-09 12:00:14 +0100

    update audit

--- app/api/v1/endpoints/affect.py
+++ app/api/v1/endpoints/affect.py
@@ -18,12 +18,12 @@ logger = setup_logger()
 
 router = APIRouter()
 
-@router.post("/affects/", response_model=AffectResponse)
+@router.post("/affects/", response_model=AffectResponse, summary="Creer une affectation", description="Permet de creer une affectation")
 def create_affectation(affect_data: AffectSchema, db: Session = Depends(get_db)):
     logger.info(f"Création d'une nouvelle affectation pour audit_id {affect_data.audit_id}")
     return create_affect(db, affect_data)
 
-@router.get("/affects/{affect_id}", response_model=AffectResponse)
+@router.get("/affects/{affect_id}", response_model=AffectResponse, summary="Lister les affectations par ID", description="Récupère une affectation par son ID avec les informations associées (auditeurs, prestataires, audit, etc.)")
 def read_affect(affect_id: int, db: Session = Depends(get_db)):
     logger.info(f"Lecture de l'affectation ID {affect_id}")
     affect = get_affect(db, affect_id)
@@ -32,40 +32,40 @@ def read_affect(affect_id: int, db: Session = Depends(get_db)):
         raise HTTPException(status_code=404, detail="Affectation non trouvée")
     return affect
 
-@router.get("/affects/", response_model=List[AffectResponse])
+@router.get("/affects/", response_model=List[AffectResponse], summary="Lister les affectations", description="Récupère la liste de toutes les affectations avec les informations associées (auditeurs, prestataires, audit...)")
 def read_affects(db: Session = Depends(get_db)):
     logger.info("Lecture de toutes les affectations")
     return list_affects(db)
 
 # Endpoints pour la gestion des auditeurs
-@router.post("/auditeurs/", response_model=AuditeurResponse)
+@router.post("/auditeurs/", response_model=AuditeurResponse, summary="Creer les auditeurs", description="Permet de creer les auditeurs")
 def create_auditor(auditeur_data: AuditeurSchema, db: Session = Depends(get_db)):
     logger.info(f"Création d’un auditeur: {auditeur_data.nom} {auditeur_data.prenom}")
     return create_auditeur(db, auditeur_data)
 
-@router.post("/prestataires/", response_model=PrestataireResponse)
+@router.post("/prestataires/", response_model=PrestataireResponse, summary="creer les prestataires", description="Permet de creer les prestataires")
 def create_prestat(prestataire_data: PrestataireSchema, db: Session = Depends(get_db)):
     logger.info(f"Création du prestataire: {prestataire_data.nom}")
     return create_prestataire(db, prestataire_data)
 
-@router.get("/auditeurs/", response_model=List[AuditeurResponse])
+@router.get("/auditeurs/", response_model=List[AuditeurResponse], summary="Lister les auditeurs", description="Récupère la liste de toutes les auditeurs avec les informations associées (nom, prenom, email...)")
 def read_auditors(db: Session = Depends(get_db)):
     logger.info("Lecture de la liste des auditeurs")
     return list_auditeurs(db)
 
 # Endpoints pour la gestion des prestataires
-@router.get("/prestataires/", response_model=List[PrestataireResponse])
+@router.get("/prestataires/", response_model=List[PrestataireResponse], summary="Lister les prestataires", description="Récupère la liste de toutes les prestataires")
 def read_prestataires(db: Session = Depends(get_db)):
     logger.info("Lecture de la liste des prestataires")
     return db.query(Prestataire).all()
 
 # Endpoints pour la gestion des IPs
-@router.get("/ips/", response_model=List[IPResponse])
+@router.get("/ips/", response_model=List[IPResponse], summary="Lister les IPs", description="Récupère la liste de toutes les IPs et leurs ports")
 def read_ips(db: Session = Depends(get_db)):
     logger.info("Lecture de la liste des IPs")
     return db.query(IP).all()
 
-@router.delete("/auditeurs/{auditeur_id}", response_model=AuditeurResponse)
+@router.delete("/auditeurs/{auditeur_id}", response_model=AuditeurResponse, summary="Supprimer un auditeur par ID", description="Permet de supprimer un auditeur par son ID de la liste de auditeurs")
 def remove_auditeur(auditeur_id: int, db: Session = Depends(get_db)):
     logger.info(f"Suppression de l’auditeur ID {auditeur_id}")
     auditeur = delete_auditeur(db, auditeur_id)
@@ -74,7 +74,7 @@ def remove_auditeur(auditeur_id: int, db: Session = Depends(get_db)):
         raise HTTPException(status_code=404, detail="Auditeur not found")
     return auditeur
 
-@router.put("/auditeurs/{auditeur_id}", response_model=AuditeurResponse)
+@router.put("/auditeurs/{auditeur_id}", response_model=AuditeurResponse, summary="Modifier un auditeur", description="Permet de modifier les informations d'un auditeur deja existant")
 def update_auditeur_endpoint(auditeur_id: int, auditeur_data: AuditeurSchema, db: Session = Depends(get_db)):
     logger.info(f"Mise à jour de l’auditeur ID {auditeur_id}")
     auditeur = update_auditeur(db, auditeur_id, auditeur_data)
--- app/core/database.py
+++ app/core/database.py
@@ -4,10 +4,20 @@ from sqlalchemy.orm import sessionmaker
 import os
 from dotenv import load_dotenv
 import logging
+from dotenv import load_dotenv
+import os
 
 load_dotenv()
 
-DATABASE_URL = os.getenv('DATABASE_URL', 'mysql+pymysql://root:JiKhNa232017*@localhost/audit_db')
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_NAME = os.getenv("DB_NAME")
+
+DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
+
+
+#DATABASE_URL = os.getenv('DATABASE_URL', 'mysql+pymysql://root:JiKhNa232017*@localhost/audit_db')
 engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
@@ -16,7 +26,7 @@ Base = declarative_base()
 
 try:
     with engine.connect() as connection:
-        logging.info("Connexion à la base de données réussie!")
+        logging.info(f"DB_USER: {DB_USER}, DB_HOST: {DB_HOST}, DB_NAME: {DB_NAME}")
 except Exception as e:
     logging.error(f"Erreur lors de la connexion à la base de données : {e}")
     raise
